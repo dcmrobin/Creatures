2699e0744ac638deecbc8e937de2d380ad14f9be
DavidCM <dcm.robin@gmail.com>
2024-03-29 21:33:03 +0000

    got mouselook

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10;
+    Vector2 rotation = Vector2.zero;
+	public float mouseSensitivity = 3;
 
     private void Update() {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-
         transform.position += new Vector3(-vertical, 0, horizontal) * speed;
+
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x += -Input.GetAxis("Mouse Y");
+        rotation.x = Mathf.Clamp(rotation.x, -15f, 15f);
+        transform.eulerAngles = new Vector2(0,rotation.y) * mouseSensitivity;
+        Camera.main.transform.localRotation = Quaternion.Euler(rotation.x * mouseSensitivity, 0, 0);
     }
 }
