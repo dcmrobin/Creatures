2c1d34e33cf7ecdd1e9c8fe9ef2744615ec6decc
DavidCM <dcm.robin@gmail.com>
2024-04-08 17:07:13 +0100

    tweaked limb

--- Assets/Scripts/Leg.cs
+++ Assets/Scripts/Leg.cs
@@ -10,6 +10,9 @@ public class Leg : MonoBehaviour
     public Transform fixedTarget;
 
     private void Update() {
+        if (limbTarget == null || fixedTarget == null)
+            return;
+
         if (Vector3.Distance(limbTarget.position, fixedTarget.position) > 0.7)
         {
             limbTarget.position = fixedTarget.position;
